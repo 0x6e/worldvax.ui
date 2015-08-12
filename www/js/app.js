26bbd4b74241feadaba6ad169f998686dfab57e6
Dennis Dunn <ansofive@gmail.com>
2015-08-11 23:10:03 -0400

    Getting the basic page together. The only thing I miss from ngMaterial is the show/hide directives.

--- www/js/app.js
+++ www/js/app.js
@@ -2,16 +2,11 @@
 
     angular.module("mir", [
         "ngRoute",
-        "ngMaterial",
         "mirSearch",
         "mirToolbar",
         "mirNavigation",
         "mirConfiguration"
-    ]).config(function($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-        .primaryPalette('green')
-        .accentPalette('blue');
-    });
+    ]);
 
     document.addEventListener('deviceready', function() {
         angular.bootstrap(document, ['mir']);
